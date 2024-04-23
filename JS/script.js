58d26fdb765198a154e32b81ba2cc5617b486c42
deepanshu14315 <deepanshu14315@gmail.com>
2024-04-23 11:49:31 +0530

    add functionality

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,15 @@
 let hamburger = document.querySelector(".hamburger");
-let closeBtn = document.querySelector("close-btn");
+let closeBtn = document.querySelector(".close-btn");
+let header = document.querySelector("header");
 
 hamburger.addEventListener('click', () => {
-    hamburger.parentElement.classList.toggle("menu-open");
-
+    hamburger.parentElement.classList.add("menu-open");
+    header.classList.add("bgwhite");
+    closeBtn.parentElement.classList.add("menu-close");
 });
-closeBtn.addEventListener('click', () => {
-    closeBtn.parentElement.classList.toggle("menu-close");
+closeBtn.addEventListener('click', ()=> {
+    closeBtn.parentElement.classList.remove("menu-close");
     hamburger.parentElement.classList.remove("menu-open");
+    header.classList.remove("bgwhite");
 
 });
-
